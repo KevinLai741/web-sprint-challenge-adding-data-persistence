4e1dbd0b7b27481ca7460574b0843cf853eb7d01
Kevin Lai <kevin.lai@gmail.com>
2022-06-17 15:02:47 -0400

    worked on project router and model

--- api/server.js
+++ api/server.js
@@ -1,1 +1,24 @@
 // build your server here and require it from index.js
+const express = require('express');
+
+const projectRouter = require('./project/router');
+
+const server = express();
+server.use(express.json());
+
+server.use('/api/projects', projectRouter);
+
+server.use('*', (req, res) => { 
+    res.json({ api: 'up' })
+})
+
+server.use((err, req, res, next) => { //eslint-disable-line
+    res.status(500).json({
+        customMessage: 'something went wrong inside the router',
+        message: err.message,
+        stack: err.stack,
+    });
+});
+
+
+module.exports = server;
